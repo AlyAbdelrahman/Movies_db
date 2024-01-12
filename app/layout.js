184e2b7828b2abff1e08a8cc947527882f9500c5
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-12 18:40:16 +0200

    adjusting redux provider place

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import ReduxProvider from '@/provider/redux/ReduxProvider'
 import Header from './components/layout/Header'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,11 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header/>
-        {children}
-        </body>
-    </html>
+      <ReduxProvider> 
+      <html lang="en">
+        <body className={inter.className}>
+          <Header/>
+          {children}
+          </body>
+      </html>
+    </ReduxProvider>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,12 +1,8 @@
 "use client"
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import HomePage from "./dashboard/page"
 
 export default function Home() {
   return (
-    <Provider store={store}>
       <div><HomePage/></div>
-    </Provider>
   )
 }
