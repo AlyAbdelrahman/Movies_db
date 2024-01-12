1d960836c390c46502530f3f5a20c8d72a666f5c
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-12 06:22:18 +0200

    adding redux provider to main component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+"use client"
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import HomePage from "./dashboard/page"
 
 export default function Home() {
   return (
-    <div><HomePage/></div>
+    <Provider store={store}>
+      <div><HomePage/></div>
+    </Provider>
   )
 }
