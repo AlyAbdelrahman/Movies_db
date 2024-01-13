4b685d452b818bb65ca2b32cc9f3e35c5f8a5ddd
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-13 02:43:17 +0200

    making movie card

---
+++ app/components/feature/movieCard/movieCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+const MovieCard = ({ movieData }) => {
+    if (movieData) {
+      const { Title, Year, imdbID, Type, Poster } = movieData;
+      return (
+          <div className="movie-card">
+          <Link href={`/movie/${imdbID}`}>
+          <img src={Poster} alt={`${Title} Poster`} className="movie-poster" />
+          <div className="movie-details">
+            <h3>{Title}</h3>
+            <p>Year: {Year}</p>
+            <p>Type: {Type}</p>
+            <p>IMDb ID: {imdbID}</p>
+          </div>
+          </Link>
+        </div>
+      );
+    } else {
+      return <div>Loading...</div>; 
+    }
+  };
+  export default MovieCard;
