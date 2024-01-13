502973a1fecde7ce59eab0830d85585e54038a6d
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-14 01:48:18 +0200

    adj tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
+    screens: {
+      'sm': '370px',
+      'md': '960px',
+      'lg': '1440px',
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
