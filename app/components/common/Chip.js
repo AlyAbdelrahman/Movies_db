7a124ea4bdf0c80d1feb95bb53138fae468c0311
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-14 01:51:18 +0200

    adj Chip component props

--- app/components/common/Chip.js
+++ app/components/common/Chip.js
@@ -1,15 +1,14 @@
-
-export default function Chip({ category, selectedChip, handleOnClick }) {
-
+export default function Chip({ category, selectedCategory, handleOnClick }) {
     return (
-        <div
-            key={category}
-            className={` leading-tight cursor-pointer rounded-full px-3 py-1 text-sm font-semibold mr-2
-      ${selectedChip === category ? 'bg-red-500 text-white' : 'bg-gray-700 text-gray-300'}
-      hover:${selectedChip !== category && 'bg-gray-600 transition-all duration-300'}`}
-            onClick={() => handleOnClick(category)}
-        >
-            {category}
-        </div>
-    )
-}
+      <div
+        key={category}
+        className={`leading-tight cursor-pointer rounded-full px-3 py-1 text-sm font-semibold mr-2
+          ${selectedCategory === category ? 'bg-red-500 text-white' : 'bg-gray-700 text-gray-300'}
+          hover:${selectedCategory !== category && 'bg-gray-600 transition-all duration-300'}`}
+        onClick={() => handleOnClick(category)}
+      >
+        {category}
+      </div>
+    );
+  }
+  
