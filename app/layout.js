d0ebe737a6d98472c7e59ca4f22a3db3ebd42bd4
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-13 19:57:27 +0200

    adding main layout tailwind style

--- app/layout.js
+++ app/layout.js
@@ -12,12 +12,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-      <ReduxProvider> 
+    <ReduxProvider>
       <html lang="en">
-        <body className={inter.className}>
-          <Header/>
-          {children}
-          </body>
+        <body className="bg-black ">
+          <div className="grid grid-cols-12 md:grid-cols-12  h-lvh">
+            {/* Left Menu */}
+            <div className="bg-gray-800 p-4 col-span-2 ">
+              <p className="text-xl font-bold">Left Menu</p>
+              {/* Add your left menu content here */}
+            </div>
+            {/* Content */}
+            <div className="col-span-8 bg-gray-700 rounded">
+              <Header />
+              {children}
+            </div>
+            {/* Right Menu */}
+            <div className="bg-gray-800 p-4 col-span-2">
+              <p className="text-xl font-bold">Right Menu</p>
+              {/* Add your right menu content here */}
+            </div>
+          </div>
+
+        </body>
       </html>
     </ReduxProvider>
   )
