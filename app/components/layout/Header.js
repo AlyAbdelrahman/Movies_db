e545061a90cc12c7c9ba73c828c0103b4c259718
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-14 20:21:35 +0200

    remove extra code from header component

--- app/components/layout/Header.js
+++ app/components/layout/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Search from '../feature/search/Search'
 
-export default function Header({}) {
+export default function Header() {
   return (
     <div>
       <Search/>
