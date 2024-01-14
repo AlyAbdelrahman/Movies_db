748df6a87e8cd3496c5326ef3143981cd96711f5
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-14 06:01:23 +0200

    remove extra log

--- app/components/feature/searchResults/SearchResults.js
+++ app/components/feature/searchResults/SearchResults.js
@@ -3,7 +3,6 @@ import MovieCard from '../movieCard/MovieCard';
 
 
 const SearchResults = ({searchResults}) => {
-  console.log('>>searchResults',searchResults)
   return (
     <div className="grid p-1 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {searchResults.map((result, index) => (
