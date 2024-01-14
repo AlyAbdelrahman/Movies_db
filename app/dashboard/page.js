7bd1c1607c33680b6236445cc201b5d7b512186e
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-14 03:19:54 +0200

    adding banner component in home page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,13 +2,20 @@
 import React, { useEffect, useReducer } from 'react';
 import SearchResults from '../components/feature/searchResults/SearchResults';
 import { useSelector } from 'react-redux';
+import Banner from '../components/feature/banner/Banner';
+import EmptyResults from '../components/feature/emptyResults/emptyResulst';
 
 export default function HomePage() {
-  const searchResults = useSelector(state => state.movies.searchResults);
-  
+    const { searchResults, currentSearchTerm } = useSelector(state => state.movies);
+
     return (
         <div>
-            {searchResults?.Search?.length > 0 ? <SearchResults searchResults={searchResults.Search} /> : 'No movies'}
+            {searchResults?.Search?.length > 0 ? <SearchResults searchResults={searchResults.Search} /> : (currentSearchTerm ? <EmptyResults /> : <Banner
+                imageUrl='/assets/background.webp'
+                movieTitle="doctor strange madness of multiverse"
+                movieCategories={['Action', 'Adventure', 'Sci-Fi']}
+            />)}
+
         </div>
     );
 }
