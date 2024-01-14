c41a84ae8f4109b2f263dcbf8a6305b08f69bd02
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-01-14 06:01:56 +0200

    adding extra data and enhance the UI for movie details page

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { getMovieDetails } from '@/app/services/services';
-import MovieCard from '@/app/components/feature/movieCard/movieCard';
 
 const MovieDetails = async ({params}) => {
     const { id } = params;
     console.log(params.id)
     const movieData = await getMovieDetails(id);
-    console.log(movieData)
+    const { Title, Plot, Released, imdbRating, Actors, Director, Writer, Poster } = movieData;
 
   if (movieData?.Title) {
     return (
-     <MovieCard movieData={movieData}/>
+        <div className="flex flex-col md:flex-row">
+        <div className="md:w-1/3">
+          {Poster && (
+            <img
+              src={Poster}
+              alt={Title}
+              className="w-full h-auto rounded-md"
+            />
+          )}
+        </div>
+        <div className="md:w-2/3 p-4">
+          <h2 className="text-3xl font-bold">{Title}</h2>
+          <p>{Plot}</p>
+          <p><strong>Released:</strong> {Released}</p>
+          <p><strong>Rating:</strong> {imdbRating}</p>
+          <p><strong>Cast:</strong> {Actors}</p>
+          <p><strong>Crew:</strong> {Director}, {Writer}</p>
+        </div>
+      </div>
     );
   } else {
     // Handle loading state more gracefully, e.g., show a spinner
